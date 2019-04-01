b757f728e4b6edf90f60e124bb1d1e38fa963ee8
darrenyong <contact.darrenyong@gmail.com>
2019-04-01 13:42:19 -0700

    Add Express and Node for backend API calls

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname));
+app.set('views', __dirname);
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+
+
+app.get('*', function (req, res) {
+  res.render('index.html');
+});
+
+app.listen(app.get('port'), function () {});
