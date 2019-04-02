860839a5a98cdf963b7836d7ecd07cf940f27422
darrenyong <contact.darrenyong@gmail.com>
2019-04-02 12:06:48 -0700

    Add function to sort data from CSV file

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,40 @@
 (function(e) {
   app.init = function() {
     d3.csv("data/valveEvents.csv", convertColumns, function(data) {
-      console.log(data);
+      processData(data);
     })
     console.log("This is working");
+  }
+
+  function processData(data) {
+    app.data = sortData(data);
+  }
+
+  function sortData(data) {
+     const nestedData = (
+      d3.nest()
+        .sortValues(function(a, b) {
+          if (a.attribute === b.attribute) {
+          return a.heroName < b.heroName ? -1 : a.heroName > b.heroName ? 1 : 0;
+        } else {
+          return a.attriute < b.attriute ? -1 : 1;
+        }})
+        .key(function(d) { return d.event })
+        .entries(data));
+
+    nestedData.forEach(function(event) {
+      let totalNumHeroes = 113;
+      let countPB = 113;
+      let rate = 0;
+      
+    })
   }
 
   function convertColumns(data) {
     return {
       // Basic Hero Info
       event: data.event,
-      hero: data.hero,
+      heroName: data.heroName,
       attribute: data.attribute,
       
       // Pick and Ban Info
