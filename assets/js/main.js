53ad8823923959d90441d01b5f8ebfa4ed3b79c2
darrenyong <contact.darrenyong@gmail.com>
2019-04-02 14:03:28 -0700

    Modify column data

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,17 +47,17 @@
   function convertColumns(data) {
     return {
       // Basic Hero Info
-      event: data.event,
+      eventName: data.event,
       heroName: data.hero,
-      attribute: data.attribute,
+      mainAttribute: data.attribute,
       
       // Pick and Ban Info
+      num_picked: +data.no_picks,
+      num_banned: +data.no_bans,
+      num_picked_banned: +data.no_picks_bans,
       pick_rate: data.pick_rate === '' ? null : parseFloat(data.pick_rate),
       ban_rate: data.ban_rate === '' ? null : parseFloat(data.ban_rate),
       pick_ban_rate: data.pick_ban_rate === '' ? null : parseFloat(data.pick_ban_rate),
-      no_picks_bans: +data.no_picks_bans,
-      no_bans: +data.no_bans,
-      no_picks: +data.no_picks,
       
       // Win Rate Info
       win_rate: parseFloat(data.win_rate)
