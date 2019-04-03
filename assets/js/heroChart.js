dc42d4371fa1d781383bfcb09c2fffabd8fc7eda
darrenyong <contact.darrenyong@gmail.com>
2019-04-02 20:22:44 -0700

    Remove unused data

--- assets/js/heroChart.js
+++ assets/js/heroChart.js
@@ -1,6 +1,4 @@
 (function(app) {
-  const HEIGHT = 500;
-  const WIDTH = 900;
   const ATTRIBUTE = {
     "Agility": { "margin": 10, "i": 0 },
     "Intelligence": { "margin": 330, "i": 36 },
@@ -10,17 +8,14 @@
   const HERO_SIZE = 32;
 
   function heroTop(data, idx) {
-    console.log(data);
     let attribute = data.mainAttribute;
     let top = HERO_MARGIN + (Math.floor( (idx - ATTRIBUTE[attribute].i) / 6)) * (HERO_MARGIN + HERO_SIZE);
-    // console.log(top + "px")
     return top + "px";
   }
 
   function heroLeft(data, idx) {
     let attribute = data.mainAttribute;
     let left = ATTRIBUTE[attribute].margin + ( (idx - ATTRIBUTE[attribute].i) % 6 ) * (HERO_MARGIN + HERO_SIZE);
-    // console.log(left + "px")
     return left + "px";
   }
 
@@ -41,11 +36,11 @@
       .classed("heroes-container", true)
       .style("opacity", 1.0)
 
-      let nData = app.filterByEvent(app.data, app.selectedEvent);
+      let filterData = app.filterByEvent(app.data, app.selectedEvent);
 
       d3.select("#hero-chart")
         .selectAll("a.hero-icon")
-        .data(nData.values, key)
+        .data(filterData.values, key)
         .enter()
         .append("a")
         .attr("id", (d) => { return app.formatName(d.heroName) })
