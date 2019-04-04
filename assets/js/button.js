b19439e793d93d16de28624564c39b74f64da961
darrenyong <contact.darrenyong@gmail.com>
2019-04-03 18:54:09 -0700

    Add functionality to unhide and hide hero details

--- assets/js/button.js
+++ assets/js/button.js
@@ -1,11 +1,35 @@
 (function(app) {
   d3.select(".details-sort")
-    .on("change", function () {
+    .on("change", function() {
       let sortButton = d3.select(this);
       let checked = sortButton.property("checked");
       app.toggleSort = checked;
       app.onEventChange();
+    })
+  
+  d3.select(".details-toggle")
+    .on("change", function() {
+      let heroDetailsButton = d3.select(this);
+      let checked = heroDetailsButton.property("checked");
+
+      if (checked) {
+        d3.select("#chart")
+          .transition()
+          .style("top", "135px");
+        d3.select(".detail-stats")
+          .transition()
+          .delay(250)
+          .style("opacity", 1.0);
+      } else {
+        d3.select("#chart")
+          .transition()
+          .delay(250)
+          .style("top", "0px");
+        d3.select(".detail-stats")
+          .transition()
+          .style("opacity", 0);
       }
-    )
+    })
+
 
 }(window.app = window.app || {}));
